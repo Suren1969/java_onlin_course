51c0ce98c1d50486e916a7cdae07bdfea8b52b92
Suren Knyazyan <gum-gyumri@yandex.ru>
2024-07-22 05:53:43 +0400

    My first git commit

---
+++ Leson7/Leson7/src/Arestages.java
@@ -0,0 +1,9 @@
+public class Arestages {
+    public static void main(String[] args) {
+        System.out.println(5+4);
+        System.out.println(5+4);
+        System.out.println(5+4);
+        System.out.println(5+4);
+
+    }
+}
